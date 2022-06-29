7c1e35b1e47d49770e15cdf8c9a3b6dbadfc3877
AntonioKal <antoniskal85@gmail.com>
2022-06-29 16:15:09 +0300

    Switch, while, for, function, stars added

---
+++ Chapter05-javascript/function.js
@@ -0,0 +1,12 @@
+function add(a,b) {
+    let sum = a + b;
+    return sum;
+}
+
+function showResults() {
+let a = 20;
+let b = 10;
+let result = add(a, b);
+
+document.write(`${a} + ${b} = ${result}`);
+}
